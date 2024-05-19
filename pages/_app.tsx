8a9ea8561b03b1409bbbedf226b2c2795726fda2
Matheus Ferreira <matheusscfr@gmail.com>
2024-05-19 14:49:13 -0300

    feat: test lib vccUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,5 @@
+
+import { StyleProvider, ThemePicker } from 'vcc-ui';
 import { HelloWorld } from "../src/components/HelloWorld";
 import "../public/css/styles.css";
 import React from "react";
@@ -5,7 +7,11 @@ import React from "react";
 function HomePage() {
   return (
     <React.StrictMode>
+      <StyleProvider>
+        <ThemePicker variant='light'>
       <HelloWorld />
+      </ThemePicker>
+      </StyleProvider>
     </React.StrictMode>
   );
 }
